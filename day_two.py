8ee8dadbcea48c11ac7f2436046f6c34612c9413
Sydney <sydneyjberry12@gmail.com>
2023-12-03 12:37:59 -0600

    day 2 pt 2 DONE

--- day_two.py
+++ day_two.py
@@ -1,5 +1,5 @@
 def day(input_file):
-    return part_one(input_file)
+    return part_two(input_file)
 
 def part_one(input_file):
     possible_games_sum = 0
@@ -12,7 +12,6 @@ def part_one(input_file):
         start_index = line.find(":") + 2
         line = line[start_index:-1]
 
-        # get blue, red, green
         red_limit = 12
         green_limit = 13
         blue_limit = 14
@@ -46,3 +45,38 @@ def part_one(input_file):
             possible_games_sum = possible_games_sum + game_num + 1
 
     return possible_games_sum
+
+
+def part_two(input_file):
+    total_power = 0
+
+    # get each game
+    for game_num, line in enumerate(input_file):
+        # parse to colon
+        start_index = line.find(":") + 2
+        line = line[start_index:-1]
+
+        red_min = 0
+        blue_min = 0
+        green_min = 0
+
+        # get each set
+        sets = line.split("; ")
+        for block_set in sets:
+            blocks = block_set.split(", ")
+
+            for block in blocks:
+                num_index = block.find(" ")
+                num = block[:num_index]
+                color = block[num_index + 1:]
+
+                if color == "red" and int(num) > red_min:
+                    red_min = int(num)
+                elif color == "green" and int(num) > green_min:
+                    green_min = int(num)
+                elif color == "blue" and int(num) > blue_min:
+                    blue_min = int(num)
+
+        total_power += (red_min * green_min * blue_min)
+
+    return total_power
